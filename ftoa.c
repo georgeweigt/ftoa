41ea2fb33e5a63e5bf221e2af1f807377f477239
georgeweigt <55809489+georgeweigt@users.noreply.github.com>
2025-10-17 16:25:43 -0700

    Update ftoa.c

--- ftoa.c
+++ ftoa.c
@@ -38,13 +38,15 @@
 
 #define DEBUG 0
 
-void unpack(float d, uint32_t *u);
+int ftoa(float d, char *buf);
+int unpack(float d, uint32_t *u, char *buf);
 void setbit(uint32_t *u, int k);
 int mulby10(uint32_t *u);
 int divby10(uint32_t *u);
 
+// 1 sign char
 // 39 leading digits max
-// 1 decimal point at buf[39]
+// 1 decimal point at buf[40]
 // 149 trailing digits max
 // 1 null terminator
 
@@ -55,7 +57,6 @@ main(int argc, char *argv[])
 {
 	int k;
 	float d;
-	uint32_t u[9]; // bigint
 
 	if (argc < 2)
 		exit(1);
@@ -65,11 +66,24 @@ main(int argc, char *argv[])
 	else
 		sscanf(argv[1], "%g", &d);
 
+	k = ftoa(d, buf);
+	puts(buf + k);
+}
+
+// returns starting index for buf
+
+int
+ftoa(float d, char *buf)
+{
+	int k;
+	uint32_t u[9]; // bigint
+
 #if DEBUG
 	printf("%e\n", d);
 #endif
 
-	unpack(d, u);
+	if (unpack(d, u, buf))
+		return 0; // inf or nan
 
 #if DEBUG
 	printf("%x %x %x %x . %x %x %x %x %x\n", u[8], u[7], u[6], u[5], u[4], u[3], u[2], u[1], u[0]);
@@ -77,28 +91,33 @@ main(int argc, char *argv[])
 
 	// trailing digits
 
-	k = 40;
+	k = 41;
 	do
 		buf[k++] = mulby10(u) + '0';
 	while (u[0] || u[1] || u[2] || u[3] || u[4]);
+	buf[k] = '\0';
 
 	// leading digits
 
-	k = 39;
+	k = 40;
 	do
 		buf[--k] = divby10(u + 5) + '0';
 	while (u[5] || u[6] || u[7] || u[8]);
 
-	// print result
+	// sign
 
-	buf[39] = '.';
-	if (*((uint32_t *) &d) & 0x80000000) // sign bit
-		putchar('-');
-	puts(buf + k);
+	if (*((uint32_t *) &d) & 0x80000000)
+		buf[--k] = '-';
+
+	buf[40] = '.';
+
+	return k;
 }
 
-void
-unpack(float d, uint32_t *u)
+// returns 1 for inf or nan
+
+int
+unpack(float d, uint32_t *u, char *buf)
 {
 	int x;
 	uint32_t m, n;
@@ -106,11 +125,14 @@ unpack(float d, uint32_t *u)
 	x = (n >> 23) & 0xff; // exponent
 	m = n << 9; // 23 bit mantissa
 	if (x == 0xff) {
-		if (m == 0)
-			(n & 0x80000000) ? puts("-inf") : puts("inf");
-		else
-			puts("nan");
-		exit(1);
+		if (m == 0) {
+			if (n & 0x80000000)
+				strcpy(buf, "-inf");
+			else
+				strcpy(buf, "inf");
+		} else
+			strcpy(buf, "nan");
+		return 1;
 	}
 	memset(u, 0, 36); // 9 words = 36 bytes
 	if (x)
@@ -121,6 +143,7 @@ unpack(float d, uint32_t *u)
 		x--;
 		m <<= 1;
 	}
+	return 0;
 }
 
 void
